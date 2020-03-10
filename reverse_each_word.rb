af46aebe48d24f63ecfaf98cdd3c0fd1a4e97666
Steven Williams <evilgeniusnyc@me.com>
2020-03-10 17:18:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
 def reverse_each_word(string)
-
-string = 'Hello there, and how are you?'
  
 string.split('').each_to_i
-    puts c
+    puts 
 }
 
 reverse_each(*args) { |item| block } → enum click to toggle source
